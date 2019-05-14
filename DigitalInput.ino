172a419f82083420895e7c1ed79dc91f1cc420fc
Rajupalepu <50615761+Rajupalepu@users.noreply.github.com>
2019-05-14 14:15:56 +0530

    Add files via upload

---
+++ DigitalInput.ino
@@ -0,0 +1,16 @@
+int switchPin = 0; //d3
+int LedPin = 16; //d0
+int switchValue;
+void setup() {
+  pinMode(LedPin,OUTPUT);
+  pinMode(switchPin,INPUT_PULLUP);
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  //Read the switch value
+  switchValue = digitalRead(switchPin);
+  digitalWrite(LedPin,!switchValue);
+}
